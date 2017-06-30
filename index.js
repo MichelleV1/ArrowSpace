9599710e9e661fe53121567de4bfd8c627a79884
MichelleV1 <michelle.marobela@gmail.com>
2017-06-30 11:57:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const ConversationV1 = require('watson-developer-cloud/conversation/v1');
 //create a new instance of the conversation service
 const conversation = new ConversationV1({
 
-    username: 'fc5c2aa9-34a8-4a98-8c6f-46ca4177e59d',
-     // username: process.env.USERNAME,
-    password: 'KzBuEbqfrL7k',
-     // password: process.env.PASSWORD,
-    version_date: ConversationV1.VERSION_DATE_2016_09_20
+    
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+       version_date: ConversationV1.VERSION_DATE_2016_09_20
 });
 
 //Make a request to watson conversation by using the message API and print watson's response
@@ -17,8 +16,7 @@ conversation.message({
     input:{
         text: ' '
     },
-   workspace_id: '1ea4cc4c-5564-4cac-9e00-8bfc60d038bd'
-    // workspace_id: process.env.WORKSPACE_ID
+      workspace_id: process.env.WORKSPACE_ID
 },
 function(err, response)
 {
